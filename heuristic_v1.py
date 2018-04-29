56515ba666cedeff16489bf97641ee24ed168bc5
Kanchan Yawalkar <kyawalkar@dhcp-rhodes-1404.eduroam.cornell.edu>
2018-04-29 16:00:10 -0400

    updated heuristic_v1

--- heuristic_v1.py
+++ heuristic_v1.py
@@ -5,8 +5,6 @@ Heuristic AI. Plays randomly.
 from objects import *
 import random
 
-random.seed()
-
 class Heuristic_v1(Player):
     def __init__(self, game, name):
         Player.__init__(self,game,name)
@@ -14,7 +12,39 @@ class Heuristic_v1(Player):
     """Computes the value h of the player's hand if the player discards
     possible_card on this turn"""
     def h(self, possible_card):
+        hVal = 0
+        newHand = self.hand.cards[:]
+        newHand.remove(possible_card)
+        for c in newHand:
+            if c.flag == 0:
+                hVal += c.value
+            elif c.flag == 1 or c.flag == 2:
+                hVal += 10
+            elif c.flag == 3:
+                hVal += 12
+            elif c.flag == 4:
+                hVal += 14
+            elif c.flag == 5:
+                hVal += 16
+        return hVal
 
+    def select_wild_color(self):
+        yellow = 0
+        red = 0
+        blue = 0
+        green = 0
+        for c in self.hand.cards:
+            if c.color == "yellow":
+                yellow += 1
+            elif c.color == "red":
+                red += 1
+            elif c.color == "blue":
+                blue += 1
+            elif c.color == "green":
+                green += 1
+        best_option = (max((yellow,"yellow"),(red,"red"),(blue,"blue"),
+            (green,"green")))[1]
+        return best_option
 
     def discard_or_draw(self):
         if not self.possible_card():
@@ -36,9 +66,8 @@ class Heuristic_v1(Player):
                 if newScore < score:
                     discard_card = possible_discards[i]
 
-            if discard_card.flag >= 4: #if wild, randomly select new color
-                i = random.randint(0,3)
-                wild_color = self.colors[i]
+            if discard_card.flag >= 4:
+                wild_color = self.select_wild_color()
             return [0,self.discard(discard_card),wild_color]
 
     def say_uno(self):
